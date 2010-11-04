523a2ce962902a0a6c16d49a1eaf6eb73717647d
Ameer Ayoub <ameer.ayoub@gmail.com>
2010-11-03 20:56:08 -0700

    Added images, formatted output, fixed maps system. Map defaults to image <mapname>.jpg

--- metadata-sc2rep.tpl.php
+++ metadata-sc2rep.tpl.php
@@ -1,7 +1,45 @@
-<h2> Metadata: </h2>
 <?php if($metadata): ?>
 <div id="sc2rep-wrapper" class="sc2rep">
-	Game Length: <?php echo($metadata['meta']['length']); ?>
+<div id="sc2rep-game-info">
+<h3> Game Info </h3>
+<table style="width: 400px; float: left; margin-right: 20px;">
+	<tr>
+		<th>Game Type</th>
+		<th>Map Name</th>
+		<th>Map Image</th>
+		<th>Length</th>
+	</tr>
+	<tr>
+		<td><?php echo($metadata['meta']['team_size']); ?></td>
+		<td><?php echo($metadata['map']['name']); ?></td>
+		<td><img src="<?php echo($minimap_url); ?>" alt="<?php echo($metadata['map']['name']); ?>" style="width: 100px;" /></td>
+		<td><?php echo($metadata['meta']['formatted_length']); ?></td>
+	</tr>
+</table>
+</div>
+<div id="sc2rep-players">
+<table style="width: 400px;">
+	<tr>
+		<th>Player</th>
+		<th>Profile</th>
+		<th>Race</th>
+		<th>APM</th>
+	</tr>
+<?php foreach($metadata['players'] as $player): ?>
+	<tr>
+		<td>
+			<span style="color: #<?php echo($player['color']); ?>; font-weight: bold; margin-left: 10px;"><?php echo($player['player_name']); ?></span>
+		</td>
+		<td>
+			<a href="<?php echo($player['sc2ranks_profile_url']); ?>"><img src="<?php echo($sc2ranks_icon_url); ?>" alt="sc2ranks profile" /></a>
+			<a href="<?php echo($player['bnet_profile_url']); ?>"><img src="<?php echo($bnet_icon_url); ?>" alt="bnet profile" /></a>
+		</td>
+		<td><img src="<?php echo($player['race_image_url']); ?>" alt="<?php echo($player['race']); ?>" /></td>
+		<td><?php echo($player['apm']); ?></td>
+	</tr>
+<?php endforeach; ?>
+</table>
+</div>
 </div>
 <?php else: ?>
 Replay Information Not Available
